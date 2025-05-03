bc6c4928a24d35cfd5aa14dcee7b00bfdb8438db
jimmyepp3 <66974824+jimmyepp3@users.noreply.github.com>
2025-05-03 14:32:43 -0500

    providing negative drame defition.

--- netlify/functions/gptHandler.js
+++ netlify/functions/gptHandler.js
@@ -1,8 +1,9 @@
-const OpenAI = require("openai");
+const { OpenAI } = require("openai");
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 });
+
 
 const messagingFrames = {
   systemPrompt: `
